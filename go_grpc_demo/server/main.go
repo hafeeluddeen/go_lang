27ea03ea06f44e374410aa9e5099df154e2eee7e
hafeeluddeen <hafeeluddeen@gmail.com>
2025-08-13 07:52:28 +0530

    Add: Initialize Server, Client and Proto Folders

---
+++ go_grpc_demo/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/hafeeluddeen/go_lang/go_grpc_demo"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8000"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	listen, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on port %v", port)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+
+	log.Printf("Server started at %v", listen.Addr())
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("Failed to start: %v", err)
+	}
+}
