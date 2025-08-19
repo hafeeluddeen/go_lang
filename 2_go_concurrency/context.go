828230087fcb043ff63a7a5a246cf7d92d2ed1b3
hafeeluddeen <hafeeluddeen@gmail.com>
2025-08-19 09:00:49 +0530

    Add: Context v1

--- 2_go_concurrency/context.go
+++ 2_go_concurrency/context.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"time"
 )
 
 func contextMain() {
@@ -45,5 +47,38 @@ func contextMain() {
 func func1(ctx context.Context, parentWg *sync.WaitGroup, stream <-chan interface{}) {
 
 	defer parentWg.Done()
+
+	var wg sync.WaitGroup
+
+	doWork := func(ctx context.Context) {
+
+		defer wg.Done()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			// ok is the language provoded variable if stream ends its set to False
+			case d, ok := <-stream:
+				if !ok {
+					fmt.Println("Channel CLosed")
+					return
+				}
+
+				fmt.Println(d)
+			}
+		}
+	}
+
+	newCtx, cancel := context.WithTimeout(ctx, time.Second*5)
+
+	defer cancel()
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go doWork(newCtx)
+	}
+
+	wg.Wait()
 
 }
